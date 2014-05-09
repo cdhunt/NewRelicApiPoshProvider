b44fe3e6f3f926747adbdd06f645b75132ca2830
Chris Hunt <cdhunt@gmail.com>
2014-05-09 15:45:47 -0400

    add Servers support

---
+++ NewRelicAPIPoshProvider/Items/Server.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NewRelicAPIPoshProvider.Items
+{
+    public class ListServers
+    {
+        public List<Server> Servers { get; set; }
+    }
+    public class Server
+    {
+        public int Id { get; set; }
+        public int Account_Id { get; set; }
+        public string Name { get; set; }
+        public string Host { get; set; }
+        public bool Reporting { get; set; }
+        public DateTime Last_Reported_At { get; set; }
+        public SummaryModel Summary { get; set; }
+
+        public class SummaryModel
+        {
+            public float Cpu { get; set; }
+            public float Cpu_Stolen { get; set; }
+            public float Disk_Io { get; set; }
+            public float Memory { get; set; }
+            public Int64 Memory_Used { get; set; }
+            public Int64 Memory_Total { get; set; }
+            public float Fullest_Disk { get; set; }
+            public Int64 Fullest_Disk_Free { get; set; }
+        }
+    }
+}
---
+++ NewRelicAPIPoshProvider/Paths/Server.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using CodeOwls.PowerShell.Paths;
+using CodeOwls.PowerShell.Provider;
+using System.Management.Automation;
+using System.Management.Automation.Provider;
+using CodeOwls.PowerShell.Paths.Processors;
+using CodeOwls.PowerShell.Provider.PathNodes;
+using System.Configuration;
+using RestSharp;
+using NewRelicApiPoshProvider;
+using NewRelicAPIPoshProvider.Items;
+
+namespace NewRelicAPIPoshProvider.Paths
+{
+    class ServersPathNode : PathNodeBase
+    {
+        private const string _servers = "https://api.newrelic.com/v2/servers.json";
+
+        public ServersPathNode()
+        {
+        }
+
+        public override IPathValue GetNodeValue()
+        {
+            return new ContainerPathValue(Name, Name);
+        }
+
+        public override string Name
+        {
+            get { return "Servers"; }
+        }
+
+        public override IEnumerable<IPathNode> GetNodeChildren(CodeOwls.PowerShell.Provider.PathNodeProcessors.IProviderContext providerContext)
+        {
+            var client = new RestClient(_servers);
+
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("X-Api-Key", providerContext.Drive.Credential.UserName);
+
+            IRestResponse<ListServers> response = client.Execute<ListServers>(request);
+
+            return from Server server in response.Data.Servers
+                   select new ServerPathNode(server) as IPathNode;
+        }
+    }
+
+    class ServerPathNode : PathNodeBase
+    {
+        private readonly Server _server;
+
+        public ServerPathNode(Server server)
+        {
+            _server = server;
+        }
+
+        public override IPathValue GetNodeValue()
+        {
+            return new LeafPathValue(_server, Name);
+        }
+
+        public override string Name
+        {
+            get { return _server.Name; }
+        }
+    }
+}
